ceb7bd780729158b913a1b7cc8570271f0662e0d
OYE\pc <cosy-@naver.com>
2026-06-26 17:47:48 +0900

    파일정리 이그노어셋

--- sg_react/src/component/container/index.jsx
+++
@@ -1,5 +0,0 @@
-/* eslint-disable react-refresh/only-export-components */
-export { default as Card } from "./Card.jsx";
-export { default as Modal, sstModal, sstModalClose } from "./Modal.jsx";
-export { default as Dialog, sstDialog, sstDialogClose } from "./Dialog.jsx";
-export { default as Acc } from "./Accordion.jsx";
